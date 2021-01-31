cbae8cfa6160534cee67434e55f9249ce139a65b
Muhammad K <akhalid.code@gmail.com>
2021-01-30 16:18:45 -0800

    add views and updated server.js

--- server.js
+++ server.js
@@ -8,3 +8,17 @@ app.use(express.static("public"));
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Set handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/booksControllers.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+	console.log("Server listening on: http://localhost:" + PORT);
+});
